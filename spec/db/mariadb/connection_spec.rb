da4a4efe8bb59085674047ec9ad0302a7117966a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-07-05 22:53:38 +1200

    Fix db test user creation.

--- spec/db/mariadb/connection_spec.rb
+++ spec/db/mariadb/connection_spec.rb
@@ -26,7 +26,7 @@ require 'async/rspec'
 RSpec.describe DB::MariaDB::Connection do
 	include_context Async::RSpec::Reactor
 	
-	subject(:connection) {described_class.new(user: 'testing', database: 'test')}
+	subject(:connection) {described_class.new(database: 'test')}
 	
 	it "should connect to local server" do
 		expect(connection.status).to include("Uptime")
