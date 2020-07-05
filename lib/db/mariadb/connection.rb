f4fc5692c9d496a97f11001388e58c539f3a3f95
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-07-05 16:43:57 +1200

    Add `Connection#call`.

--- lib/db/mariadb/connection.rb
+++ lib/db/mariadb/connection.rb
@@ -85,6 +85,18 @@ module DB
 			def next_result
 				@native.next_result
 			end
+			
+			def call(statement, streaming: false)
+				@native.send_query(statement)
+				
+				last_result = nil
+				
+				while result = @native.next_result
+					last_result = result
+				end
+				
+				return last_result
+			end
 		end
 	end
 end
