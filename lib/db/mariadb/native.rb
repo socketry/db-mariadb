c96f23f77813c881dabfb3cb51eb635e015c13a5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-04-04 11:13:56 +1200

    Use `ffi_load_failure` to help users to install the right packages.

--- lib/db/mariadb/native.rb
+++ lib/db/mariadb/native.rb
@@ -30,7 +30,28 @@ module DB
 			
 			ffi_load('mariadb') ||
 				ffi_load_using_config_tool(%w{mariadb_config --libs}) ||
-				ffi_load_using_config_tool(%w{mysql_config --libs})
+				ffi_load_using_config_tool(%w{mysql_config --libs}) ||
+				ffi_load_failure(<<~EOF)
+					Unable to load libmariadb!
+					
+					## Ubuntu
+					
+						sudo apt-get install libmariadb-dev
+					
+					## Arch Linux
+					
+						sudo pacman -S mariadb
+					
+					## MacPorts
+					
+						sudo port install mariadb-10.5
+						sudo port select --set mysql mariadb-10.5
+					
+					## Homebrew
+					
+						brew install mariadb
+					
+				EOF
 		end
 	end
 end
